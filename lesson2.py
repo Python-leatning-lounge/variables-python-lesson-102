4633dec47cfb9e04707e283d9ced2ff0ba0fc81c
RED <121568700+Cesar-Gabriel@users.noreply.github.com>
2025-03-16 01:30:22 -0600

    Create lesson2.py

---
+++ lesson2.py
@@ -0,0 +1,8 @@
+# All right gramps, its time for a simple challenge, variables
+# Variables is the coding elemental to read data world wide
+# To start we will need to do is this
+name = John Doe
+# A variable on python is when you put a wordkey before a = and putting the message after the =
+# Now let's print this with what we learned on lesson 101
+name = John Doe
+print ("Hello," + name)
